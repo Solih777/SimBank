f5651e7959b6b26ae675e9f338556109d2d11d57
Work <toktogonovsalih78@gmail.com>
2026-01-14 10:24:45 +0600

    Resloved conflict

---
+++ src/main/java/kg_sim_bank_delivery/Delivery.java
@@ -0,0 +1,8 @@
+package kg_sim_bank_delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+        System.out.println("Delivery yandex");
+        System.out.println("Delivery Glovo");
+    }
+}
