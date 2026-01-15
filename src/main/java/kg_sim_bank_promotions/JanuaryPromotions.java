a72cda10cf021ca6d24dd196b8246c5a7b784334
Work <toktogonovsalih78@gmail.com>
2026-01-15 20:26:05 +0600

    January promo123
    january promotion
    
    January promotion 2
    
    January promotion 3
    
    January promotion 4
    
    January promotion 5
    
    January promotion 6
    
    January promotion 7
    
    A

---
+++ src/main/java/kg_sim_bank_promotions/JanuaryPromotions.java
@@ -0,0 +1,15 @@
+package kg_sim_bank_promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+        System.out.println("January promotion");
+        System.out.println("January promotion123");
+        System.out.println("January promotion12345");
+        System.out.println("January promotion1234567");
+        System.out.println("January promotion123456789");
+        System.out.println("January promotion1234567890");
+        System.out.println("January promotion123456789010");
+        System.out.println("January promotion12345678901011");
+        System.out.println("January promotion1234567890101112");
+    }
+}
